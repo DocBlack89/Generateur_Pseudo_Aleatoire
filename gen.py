2adbf26bb5f605ff3078803f66920a7b4fefe268
DocBlack89 <antoinegibier10@hotmail.fr>
2018-04-30 11:06:41 +0200

    jesaispas

--- gen.py
+++ gen.py
@@ -1,32 +1,40 @@
 import datetime
 from collections import Counter
 import re
+import csv
+import time
 
 def gen1():
-    a = 6
-    b = 2
-    m = 24
+    a = 16807
+    b = 0
+    m = 31
     # k = datetime.datetime.now()
     # k = str(k)
     # ms = re.findall("([0-9]{6})", k)
     # ms1 = ms[0]
     # ms2 = int(ms1)
     k = 5
-    f = ((a*k)+b)%(2**m)
+    f = ((a*k)+b)%((2**m)-1)
     return f
 
 
 def alea():
     k = int(gen1())
     liste = [k]
-    for i in range(100):
-        a = 6
-        b = 2
-        m = 24
-        f = ((a*k)+b)%(2**m)
+    temps = time.time()
+    temps1 = time.time()
+    while (temps1 < (temps+300)):
+        a = 16807
+        b = 0
+        m = 31
+        f = ((a*k)+b)%((2**m)-1)
         k = f
         liste.append(k)
+        g = open("liste.txt", "a")
+        nb = str(k)+"\n"
+        g.write(nb)
+        temps1 = time.time()    
     c = Counter(liste)
+    g.write(c)
     print(c)
-    # Occurence après 23 fois
 alea()
---
+++ nombre.py
@@ -0,0 +1,4 @@
+for i in range(461000):
+    f = open("nombre.txt", "a")
+    mot = str(i)+"\n"
+    f.write(mot)
