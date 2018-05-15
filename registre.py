cb81563fe9908089d324a0fff78c2719d0067e16
DocBlack89 <antoinegibier10@hotmail.fr>
2018-05-15 14:48:59 +0200

    registre fin

--- registre.py
+++ registre.py
@@ -3,14 +3,21 @@ import re
 
 def xor():
     nb = "10001101"
-    b8 = nb[7]
-    b5 = nb[4]
-    b3 = nb[2]
-    print(b8)
-    print(b5)
-    print(b3)
-    y = int(b3,2) ^ int(b5,2) ^ int(b8,2)
+    nb_liste = []
+    for i in range(len(nb)):
+        nb_liste.append(nb[i])
+    print(nb_liste)
+
+    b_8 = nb_liste[7]
+    b_5 = nb_liste[4]
+    b_3 = nb_liste[2]
+    y = int(b_3,2) ^ int(b_5,2) ^ int(b_8,2)
     print('{0:b}'.format(y))
-    nb.del[7]
-    nb.add[]
+    del nb_liste[7]
+    nb_liste.insert(0,y)
+    print(nb_liste)
+    xn = y*2**7 + int(nb_liste[1])*2**6 + int(nb_liste[2])*2**5 + int(nb_liste[3])*2**4 + int(nb_liste[4])*2**3 + int(nb_liste[5])*2**2 + int(nb_liste[6])*2**1 + int(nb_liste[7])
+    xn = xn/(2**8)
+    print(xn)
+
 xor()
